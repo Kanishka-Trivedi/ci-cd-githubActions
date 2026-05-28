3d0b162d5e324dd58d019e9eaa1d341684b03f7a
Kanishka-Trivedi <kally651997@gmail.com>
2026-05-28 10:21:34 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 8080;
+app.get("/", (req, res) => {
+    return res.json({ msg: "Hello From the server."});
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
